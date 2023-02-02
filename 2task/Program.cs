4594aedbe2538c5b55cb67beeadaea119fc4acbc
Fyodor <feodorinin@gmail.com>
2023-02-02 17:20:48 +0300

    Модифицировал решение 1 задачи, добавил решения 2 и 3 задач

---
+++ 2task/Program.cs
@@ -0,0 +1,72 @@
+﻿/* Задайте прямоугольный двумерный массив. Напишите
+программу, которая будет находить строку 
+с наименьшей суммой элементов.
+
+Например, задан массив:
+
+1 4 7 2
+
+5 9 2 3
+
+8 4 2 4
+
+5 2 6 7
+
+Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка */
+
+Console.Clear();
+
+Console.WriteLine("Введите границы массива");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int min = Convert.ToInt32(Console.ReadLine());
+int max = Convert.ToInt32(Console.ReadLine());
+double[,] myArray = array(a, b, min, max);
+ShowArray(myArray);
+MinLine(myArray);
+
+double[,] array(int size1, int size2, int min, int max)
+{
+    double[,] massiv = new double[size1, size2];
+    for (int i = 0; i < size1; i++)
+    {
+        for (int j = 0; j < size2; j++)
+        {
+            massiv[i, j] = new Random().NextDouble() * (max - min);
+        }
+    }
+    return massiv;
+}
+void ShowArray(double[,] massive)
+{
+    for (int i = 0; i < massive.GetLength(0); i++)
+    {
+        for (int j = 0; j < massive.GetLength(1); j++)
+        {
+            Console.Write($"{massive[i, j]:f5} ");
+        }
+        Console.WriteLine();
+    }
+}
+void MinLine(double[,] array)
+{
+    double[] massive = new double[array.GetLength(0)];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            massive[i] += array[i, j];
+        }
+    }
+    double min = massive[0];
+    int minIndex = 0;
+    for (int k = 0; k < massive.Length; k++)
+    {
+        if (massive[k] < min)
+        {
+            min = massive[k];
+            minIndex = k + 1;
+        }
+    }
+    Console.WriteLine($"Минимальная сумма элеентов равная {min} находится в строке {minIndex}");
+}
---
+++ 3task/Program.cs
@@ -0,0 +1,75 @@
+﻿/* Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+Например, даны 2 матрицы:
+2 4 | 3 4
+3 2 | 3 3
+Результирующая матрица будет:
+18 20
+15 18 */
+
+Console.Clear();
+
+Console.WriteLine("Введите количество строк 1 матрицы");
+int row1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов 1 матрицы");
+int column1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество строк 2 матрицы");
+int row2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите количество столбцов 2 матрицы");
+int column2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите границы значений в матрице");
+int min = Convert.ToInt32(Console.ReadLine());
+int max = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+if(column1!=row2)
+{
+    Console.WriteLine("Такие матрицы нельзя умножать");
+}
+else
+{
+int[,] array1 = array(row1, column1, min, max);
+ShowArray(array1);
+Console.WriteLine();
+int[,] array2 = array(row2, column2, min, max);
+ShowArray(array2);
+Console.WriteLine();
+ShowArray(matrix(array1,array2));
+}
+
+int[,] array(int size1, int size2, int min, int max)
+{
+    int[,] massiv = new int[size1, size2];
+    for (int i = 0; i < size1; i++)
+    {
+        for (int j = 0; j < size2; j++)
+        {
+            massiv[i, j] = new Random().Next(min, max + 1);
+        }
+    }
+    return massiv;
+}
+void ShowArray(int[,] massive)
+{
+    for (int i = 0; i < massive.GetLength(0); i++)
+    {
+        for (int j = 0; j < massive.GetLength(1); j++)
+        {
+            Console.Write($"{massive[i, j]:f5} ");
+        }
+        Console.WriteLine();
+    }
+}
+int[,] matrix(int[,] massiv1, int[,] massiv2)
+{
+    int[,] multiMatrix = new int[massiv1.GetLength(0), massiv2.GetLength(1)];
+    for (int i = 0; i < multiMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < multiMatrix.GetLength(1); j++)
+        {
+            for (int y = 0; y < massiv1.GetLength(1); y++)
+            {
+                multiMatrix[i, j] += massiv1[i, y] * massiv2[y, j];
+            }
+        }
+    }
+    return multiMatrix;
+}
